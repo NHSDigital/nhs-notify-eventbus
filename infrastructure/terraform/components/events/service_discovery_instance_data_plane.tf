14ade34bfa67e9cdade371320e9d9cd820757bac
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-01-22 10:46:02 +0000

    CCM-8236 POC2

--- infrastructure/terraform/components/events/service_discovery_instance_data_plane.tf
+++ infrastructure/terraform/components/events/service_discovery_instance_data_plane.tf
@@ -1,11 +1,10 @@
 resource "aws_service_discovery_instance" "data_plane" {
   service_id   = aws_service_discovery_service.main.id
-  instance_id  = "data-plane-queue"
+  instance_id  = "data-plane-event-bus"
 
   attributes = {
-    arn         = module.sqs_queue_data_plane_ingest.sqs_queue_arn
-    queue_url   = module.sqs_queue_data_plane_ingest.sqs_queue_url
-    description = "SQS queue in Cloud Map"
+    arn         = aws_cloudwatch_event_bus.data_plane.arn
+    description = "EventBus in Cloud Map"
     kms_key_arn = module.kms.key_arn
     kms_key_id  = module.kms.key_id
   }
