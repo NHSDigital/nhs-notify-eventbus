421a72ff92bb0cb28af0fb8b0fdac171a9b65d79
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-03-10 14:38:45 +0000

    CCM-8840 Adding S3 access logging

--- infrastructure/terraform/components/acct/module_s3bucket_access_logs.tf
+++ infrastructure/terraform/components/acct/module_s3bucket_access_logs.tf
@@ -1,7 +1,7 @@
-module "s3bucket_lambda_artefacts" {
+module "s3bucket_access_logs" {
   source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/s3bucket?ref=v1.0.8"
 
-  name = "lambda-artefacts"
+  name = "access-logs"
 
   aws_account_id = var.aws_account_id
   region         = var.region
@@ -35,12 +35,8 @@ module "s3bucket_lambda_artefacts" {
   ]
 
   policy_documents = [
-    data.aws_iam_policy_document.s3bucket_lambda_artefacts.json
+    data.aws_iam_policy_document.s3bucket_access_logs.json
   ]
-
-  bucket_logging_target = {
-    bucket = module.s3bucket_access_logs.id
-  }
 
   public_access = {
     block_public_acls       = true
@@ -51,11 +47,11 @@ module "s3bucket_lambda_artefacts" {
 
 
   default_tags = {
-    Name = "Lambda function artefact bucket"
+    Name = "S3 bucket access logs"
   }
 }
 
-data "aws_iam_policy_document" "s3bucket_lambda_artefacts" {
+data "aws_iam_policy_document" "s3bucket_access_logs" {
   statement {
     sid    = "DontAllowNonSecureConnection"
     effect = "Deny"
@@ -65,8 +61,8 @@ data "aws_iam_policy_document" "s3bucket_lambda_artefacts" {
     ]
 
     resources = [
-      module.s3bucket_lambda_artefacts.arn,
-      "${module.s3bucket_lambda_artefacts.arn}/*",
+      module.s3bucket_access_logs.arn,
+      "${module.s3bucket_access_logs.arn}/*",
     ]
 
     principals {
@@ -123,6 +119,27 @@ data "aws_iam_policy_document" "s3bucket_lambda_artefacts" {
       type        = "AWS"
       identifiers = [
         "arn:aws:iam::${var.aws_account_id}:root"
+      ]
+    }
+  }
+
+  statement {
+    sid    = "AllowS3AccessLogging"
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${module.s3bucket_access_logs.arn}/*",
+    ]
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "logging.s3.amazonaws.com",
       ]
     }
   }
