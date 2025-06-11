14f445190210ceb506c99ebf8a0a30b9cc06f09f
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-11 14:24:15 +0100

    CCM-10258: SMS Nudge Component Add Outputs

--- infrastructure/terraform/components/events/outputs.tf
+++ infrastructure/terraform/components/events/outputs.tf
@@ -1,12 +1,13 @@
-output "event_buses" {
+output "control_plane_event_bus" {
   value = {
-    control_plane = {
-      name = aws_cloudwatch_event_bus.control_plane.name
-      arn  = aws_cloudwatch_event_bus.control_plane.arn
-    }
-    data_plane = {
-      name = aws_cloudwatch_event_bus.data_plane.name
-      arn  = aws_cloudwatch_event_bus.data_plane.arn
-    }
+    name = aws_cloudwatch_event_bus.control_plane.name
+    arn  = aws_cloudwatch_event_bus.control_plane.arn
+  }
+}
+
+output "data_plane_event_bus" {
+  value = {
+    name = aws_cloudwatch_event_bus.data_plane.name
+    arn  = aws_cloudwatch_event_bus.data_plane.arn
   }
 }
