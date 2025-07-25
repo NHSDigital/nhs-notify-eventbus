534c2cf5d2fb940dcf2d33d665bd3a20cdc52c05
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-07-25 10:21:10 +0100

    CCM-11362: SMS Nudge Go Live - Wrap Flatten in Distinct

--- infrastructure/terraform/components/events/cloudwatch_event_bus_policy_control_plane.tf
+++ infrastructure/terraform/components/events/cloudwatch_event_bus_policy_control_plane.tf
@@ -19,12 +19,10 @@ data "aws_iam_policy_document" "control_plane_ingest" {
       test     = "ArnLike"
       variable = "aws:PrincipalArn"
 
-      values = [
-        distinct(flatten([
-          formatlist("arn:aws:iam::%s:role/comms-*-api-event-publisher", var.event_publisher_account_ids),
-          formatlist("arn:aws:iam::%s:role/nhs-notify-*-eventpub", var.event_publisher_account_ids)
-        ]))
-      ]
+      values = distinct(flatten([
+        formatlist("arn:aws:iam::%s:role/comms-*-api-event-publisher", var.event_publisher_account_ids),
+        formatlist("arn:aws:iam::%s:role/nhs-notify-*-eventpub", var.event_publisher_account_ids)
+      ]))
     }
   }
 }
