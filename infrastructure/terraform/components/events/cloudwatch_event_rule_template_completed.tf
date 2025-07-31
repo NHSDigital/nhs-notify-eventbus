caa032ea56ed0873154240e9d6794f000fcf9740
Chris Elliott <chris.elliott17@nhs.net>
2025-07-31 12:28:19 +0100

    CCM-7943: Add templates events

---
+++ infrastructure/terraform/components/events/cloudwatch_event_rule_template_completed.tf
@@ -0,0 +1,20 @@
+resource "aws_cloudwatch_event_rule" "template_completed" {
+  name           = "${local.csi}-template-completed"
+  description    = "Event rule for inbound TemplateCompleted events"
+  event_bus_name = aws_cloudwatch_event_bus.data_plane.name
+
+  event_pattern = jsonencode({
+    "detail": {
+      "type": [
+        "uk.nhs.notify.template-management.TemplateCompleted.v1"
+      ]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "template_completed_notify_core_templates_queue" {
+  rule           = aws_cloudwatch_event_rule.template_completed.name
+  arn            = var.event_target_arns["notify_core_templates_queue"]
+  target_id      = "notify-core-templates-queue"
+  event_bus_name = aws_cloudwatch_event_bus.control_plane.name
+}
