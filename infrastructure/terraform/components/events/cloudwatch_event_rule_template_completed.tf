c21880dec12bcdaa75912aae96a9087270f76c19
Chris Elliott <chris.elliott17@nhs.net>
2025-08-07 17:19:07 +0100

    CCM-7943: Conditional deploy of event targets

--- infrastructure/terraform/components/events/cloudwatch_event_rule_template_completed.tf
+++ infrastructure/terraform/components/events/cloudwatch_event_rule_template_completed.tf
@@ -9,16 +9,18 @@ resource "aws_cloudwatch_event_rule" "template_completed" {
         "uk.nhs.notify.template-management.TemplateCompleted.v1"
       ]
     },
-      "source": [{
-        "wildcard": "//notify.nhs.uk/app/*/${var.template_management_source_environment}",
-      }]
+    "source" : [{
+      "wildcard" : "//notify.nhs.uk/app/*/${var.template_management_source_environment}",
+    }]
   })
 }
 
 resource "aws_cloudwatch_event_target" "template_completed_notify_core_templates_queue" {
+  count = length(try(event_target_arns["notify_core_templates_queue"], []))
+
   rule           = aws_cloudwatch_event_rule.template_completed.name
-  arn            = var.event_target_arns["notify_core_templates_queue"]
-  target_id      = "notify-core-templates-queue"
+  arn            = event_target_arns["notify_core_templates_queue"][count.index]
+  target_id      = "notify-core-templates-queue-${count.index}"
   event_bus_name = aws_cloudwatch_event_bus.control_plane.name
   role_arn       = aws_iam_role.send_to_notify_core_templates_queue.arn
   input_path     = "$.detail"
