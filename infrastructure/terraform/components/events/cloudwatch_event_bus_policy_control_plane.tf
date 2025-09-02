d4793e886357820415baa3bedc92b3f816361ff2
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-09-02 14:17:06 +0100

    CCM-11723: Send Template Control Events from Prod to Test

--- infrastructure/terraform/components/events/cloudwatch_event_bus_policy_control_plane.tf
+++ infrastructure/terraform/components/events/cloudwatch_event_bus_policy_control_plane.tf
@@ -17,14 +17,22 @@ data "aws_iam_policy_document" "control_plane_ingest" {
 
     principals {
       type        = "AWS"
-      identifiers = distinct(formatlist("arn:aws:iam::%s:root", var.event_publisher_account_ids))
+      identifiers = distinct(concat(
+        formatlist("arn:aws:iam::%s:root", var.event_publisher_account_ids),
+        var.template_control_cross_account_source != null ? ["arn:aws:iam::${var.template_control_cross_account_source.account_id}:root"] : []
+      ))
     }
     condition {
       test     = "ArnLike"
       variable = "aws:PrincipalArn"
       values = distinct(flatten([
         formatlist("arn:aws:iam::%s:role/comms-*-api-event-publisher", var.event_publisher_account_ids),
-        formatlist("arn:aws:iam::%s:role/nhs-notify-*-eventpub", var.event_publisher_account_ids)
+        formatlist("arn:aws:iam::%s:role/nhs-notify-*-eventpub", var.event_publisher_account_ids),
+        (
+          var.template_control_cross_account_source != null
+        ) ? [
+          "arn:aws:iam::${var.template_control_cross_account_source.account_id}:role/nhs-${var.template_control_cross_account_source.environment}-events-template-control-cross-account"
+        ] : []
       ]))
     }
   }
---
+++ infrastructure/terraform/components/events/module_template_control_cross_account_dlq.tf
@@ -0,0 +1,29 @@
+module "template_control_cross_account_dlq" {
+  count = ( var.template_control_cross_account_target != null ) ? 1 : 0
+  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.20/terraform-sqs.zip"
+
+  aws_account_id  = var.aws_account_id
+  component       = var.component
+  environment     = var.environment
+  project         = var.project
+  region          = var.region
+  name            = "template-control-cross-account-dlq"
+  sqs_kms_key_arn = module.kms.key_arn
+
+  sqs_policy_overload = data.aws_iam_policy_document.template_control_cross_account_dlq_allow_eventbridge[0].json
+}
+
+data "aws_iam_policy_document" "template_control_cross_account_dlq_allow_eventbridge" {
+  count = ( var.template_control_cross_account_target != null ) ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [module.template_control_cross_account_dlq[0].sqs_queue_arn]
+  }
+}
