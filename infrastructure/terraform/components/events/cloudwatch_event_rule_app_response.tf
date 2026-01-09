bb4cc3288e5f416bc6423af7035ca1e6079c3969
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-01-09 11:54:57 +0000

    CCM-13772: App Response - SQS Event Target

---
+++ infrastructure/terraform/components/events/cloudwatch_event_rule_app_response.tf
@@ -0,0 +1,23 @@
+resource "aws_cloudwatch_event_rule" "app_response_request" {
+  name           = "${local.csi}-app-response-request"
+  description    = "App Response event rule for inbound request events"
+  event_bus_name = aws_cloudwatch_event_bus.data_plane.name
+
+  event_pattern = jsonencode({
+    "detail" : {
+      "type" : [
+        "uk.nhs.notify.app-response.*"
+      ],
+      "dataschemaversion" : [{
+        "prefix" : "1."
+      }]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "app_response_request" {
+  rule           = aws_cloudwatch_event_rule.app_response_request.name
+  arn            = var.event_target_arns["app_response_request"]
+  target_id      = "app-response-request-events-target"
+  event_bus_name = aws_cloudwatch_event_bus.data_plane.name
+}
