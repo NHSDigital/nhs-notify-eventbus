e7c9cf2dfbd97f83ab70130bdf501cc55b2f3a88
sidnhs <siddharth.nair1@nhs.net>
2026-02-26 15:19:03 +0000

    CCM-14452: Fix eventbus logs delivery (#101)

--- infrastructure/terraform/components/events/cloudwatch_log_group_data_plane.tf
+++ infrastructure/terraform/components/events/cloudwatch_log_group_data_plane.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "data_plane" {
-  name              = "/aws/vendedlogs/events/event-bus/${local.csi}-data"
+  name              = "/aws/vendedlogs/events/event-bus/${aws_cloudwatch_event_bus.data_plane.name}"
   retention_in_days = var.log_retention_in_days
   kms_key_id        = module.kms.key_arn
 }
