98d662b1dbad638f52605694c4043dafc6db632c
Aiden Vaines <54067008+aidenvaines-cgi@users.noreply.github.com>
2026-03-03 11:38:04 +0000

    CCM-14044 Adding anomaly alarms (#103)
    * CCM-14044 Adding anomaly alarms
    
    * CCM-14044 Adding anomaly alarms
    
    * CCM-14044 Adding anomaly alarms

---
+++ infrastructure/terraform/components/events/cloudwatch_metric_alarm_control_plane_ingestion_anomaly.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudwatch_metric_alarm" "control_plane_ingestion_anomaly" {
+  count = var.enable_event_anomaly_detection ? 1 : 0
+
+  alarm_name          = "${local.csi}-control-plane-ingestion-anomaly"
+  alarm_description   = "RELIABILITY: Detects anomalous patterns in events ingested to the control plane event bus"
+  comparison_operator = "LessThanLowerOrGreaterThanUpperThreshold"
+  evaluation_periods  = var.event_anomaly_evaluation_periods
+  threshold_metric_id = "ad1"
+  treat_missing_data  = "notBreaching"
+
+  metric_query {
+    id          = "m1"
+    return_data = true
+
+    metric {
+      metric_name = "Ingestion"
+      namespace   = "AWS/Events"
+      period      = var.event_anomaly_period
+      stat        = "Sum"
+
+      dimensions = {
+        EventBusName = aws_cloudwatch_event_bus.control_plane.name
+      }
+    }
+  }
+
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, ${var.event_anomaly_band_width})"
+    label       = "Ingestion (expected)"
+    return_data = true
+  }
+
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${local.csi}-control-plane-ingestion-anomaly"
+    }
+  )
+}
--- infrastructure/terraform/components/events/outputs.tf
+++ infrastructure/terraform/components/events/outputs.tf
@@ -11,3 +11,35 @@ output "data_plane_event_bus" {
     arn  = aws_cloudwatch_event_bus.data_plane.arn
   }
 }
+
+output "data_plane_ingestion_anomaly_alarm" {
+  description = "Data plane ingestion anomaly detection alarm details"
+  value = var.enable_event_anomaly_detection ? {
+    arn  = aws_cloudwatch_metric_alarm.data_plane_ingestion_anomaly[0].arn
+    name = aws_cloudwatch_metric_alarm.data_plane_ingestion_anomaly[0].alarm_name
+  } : null
+}
+
+output "data_plane_invocations_anomaly_alarm" {
+  description = "Data plane invocations anomaly detection alarm details"
+  value = var.enable_event_anomaly_detection ? {
+    arn  = aws_cloudwatch_metric_alarm.data_plane_invocations_anomaly[0].arn
+    name = aws_cloudwatch_metric_alarm.data_plane_invocations_anomaly[0].alarm_name
+  } : null
+}
+
+output "control_plane_ingestion_anomaly_alarm" {
+  description = "Control plane ingestion anomaly detection alarm details"
+  value = var.enable_event_anomaly_detection ? {
+    arn  = aws_cloudwatch_metric_alarm.control_plane_ingestion_anomaly[0].arn
+    name = aws_cloudwatch_metric_alarm.control_plane_ingestion_anomaly[0].alarm_name
+  } : null
+}
+
+output "control_plane_invocations_anomaly_alarm" {
+  description = "Control plane invocations anomaly detection alarm details"
+  value = var.enable_event_anomaly_detection ? {
+    arn  = aws_cloudwatch_metric_alarm.control_plane_invocations_anomaly[0].arn
+    name = aws_cloudwatch_metric_alarm.control_plane_invocations_anomaly[0].alarm_name
+  } : null
+}
